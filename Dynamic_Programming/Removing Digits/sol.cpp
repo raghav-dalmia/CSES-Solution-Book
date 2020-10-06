d0a3421200521c0c6283032dff4f743128faef50
Nishant Agrawal <nishant.agrawal0013@gmail.com>
2020-10-06 23:45:32 +0530

    Resolve bug in Removing digits problem and add new category for range queries problem

--- Dynamic_Programming/Removing Digits/sol.cpp
+++ Dynamic_Programming/Removing Digits/sol.cpp
@@ -1,4 +1,4 @@
-#include<bits./stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define int long long
 
@@ -6,13 +6,23 @@ signed main(){
     int n;
     cin>>n;
     int dp[n+1];
-    memset(dp,63,sizeof(dp));
+
+    // Initialising dp array to infinity
+    // dp[i] will store minimum number of steps to decrease i to 0 using the given operation
+    for(int i=0;i<=n;i++)
+        dp[i]=1e18;
+
+    // 0 steps are needed for 0 to become 0
     dp[0]=0;
     for(int i=1;i<=n;i++){
         int dig;
         int j=i;
+        
+        // Iterating over each digit to subtract from the number and then minimising the answer for 'i'
         while(j>0){
             dig=j%10;
+
+            // if taking minimum of current value of steps for i and one plus value obtained by subtracting the current digit
             dp[i]=min(dp[i],1+dp[i-dig]);
             j/=10;
         }
