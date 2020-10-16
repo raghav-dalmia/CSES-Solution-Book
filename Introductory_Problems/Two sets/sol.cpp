1ebedf2962638c6bbb3fa8c2d0d1bd718c1377ad
Shivani <dalmiashivani1203@gmail.com>
2020-10-17 00:11:57 +0530

    Comments added

--- Introductory_Problems/Two sets/sol.cpp
+++ Introductory_Problems/Two sets/sol.cpp
@@ -6,6 +6,7 @@ signed main()
     int n;
     cin>>n;
 
+    // Sum of all the numbers
     int total_sum = (n*(n+1))/2;
     
     if (total_sum%2 != 0)
@@ -14,11 +15,21 @@ signed main()
     {
         cout<<"YES"<<endl;
 
+        // Sum of one set
         int s1 = total_sum/2;
+
+        // vector which tells us whether the number belongs to set 1 or set 2 
         vector<int>v(n+1,0);
+        
         int counter = n;
+
+        // size of first set
         int size1=0;
+
+        // seize of second set
         int size2=0;
+        
+        // Identifying elements for first set
         while(s1!=0)
         {
             if (s1-counter < 0)
@@ -33,6 +44,7 @@ signed main()
             v[s1]=1;
             size1++;
         }
+        
         size2 = n-size1;
         cout<<size1<<endl;
         for (int i=1;i<=n;i++)
@@ -46,8 +58,6 @@ signed main()
             if (v[i] == 0)
             cout<<i<<" ";
         }
-        cout<<endl;
-        
+        cout<<endl;      
     }
-    
 }
