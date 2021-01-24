7475dcf37fab3cb83f8921580dd43eed10ce0d73
Amisha Mohapatra <mohapatraamisha641@gmail.com>
2021-01-24 12:28:01 +0530

    Update sol.java

--- Introductory_Problems/Increasing Array/sol.java
+++ Introductory_Problems/Increasing Array/sol.java
@@ -17,9 +17,9 @@ class Sol{
        int ans=0;
     for (int i=1;i<n;i++)
     {
-        if (a[i] < a[i-1]) {
-        ans += (a[i-1]-a[i]);
-        a[i] = a[i-1];
+        if (array[i] < array[i-1]) {
+        ans += (array[i-1]-array[i]);
+        array[i] = array[i-1];
         }
     }
     cout<<ans<<endl;
